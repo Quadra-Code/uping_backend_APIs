74bf7396ab0959daf19b0f9fc265146a2ddfeb07
kareemelsonbaty <71130316+Kareem7777@users.noreply.github.com>
2024-06-20 00:40:22 +0300

    Static Media Modifications.
    Added static url routing and modified media folder name.

--- uping/uping/settings.py
+++ uping/uping/settings.py
@@ -136,8 +136,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 LOGIN_URL = '/login/'
-MEDIA_URL = '/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
